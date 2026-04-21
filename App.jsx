1bd9c5b4f481defbaa9122ab4493fc4a2ea8230d
khaledyattou0559-hub <khaledyattou0559@gmail.com>
2026-04-21 19:47:05 +0100

    Push React App.jsx file for educational platform

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Educational Platform</h1>
+      <p>This is a React app for displaying educational resources.</p>
+    </div>
+  );
+};
+
+export default App;
